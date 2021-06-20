96e49252911ba43b334f032aff761394b900278b
Madushan Nipuna Shantharathna <madushannipuna1997@gmail.com>
2021-06-20 12:55:05 +0530

    interface Finished

---
+++ src/Observer/App.java
@@ -0,0 +1,12 @@
+package Observer;
+
+import javax.swing.*;
+
+public class App {
+    private JButton subscribeButton;
+    private JCheckBox industryCheckBox;
+    private JCheckBox healthCheckBox;
+    private JCheckBox politicalCheckBox;
+    private JCheckBox fashionCheckBox;
+    private JCheckBox sportsCheckBox;
+}
